b5362b34f0532e4d1b14a262a4068d2eddfe6e45
Jessica Pinta <enrique.glez.jai@gmail.com>
2026-03-02 11:21:48 +0100

    versión final

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ function namesdom() {
     let adj = ['great', 'big'];
     let noun = ['jogger', 'racoon'];
 
-for (let index = 0; index < pronoun.length; index++) {
-    for (let i = 0; i < adj.length; i++)
-    for (let in = 0; in < noun.length; in+
-)
-}
+    for (let index = 0; index < pronoun.length; index++) {
+        for (let i = 0; i < adj.length; i++) {
+            for (let e = 0; e < noun.length; e++) {
+                console.log(pronoun[index] + adj[i] + noun[e] + ".com");
+            }
+        }
+    }
 }
 
+console.log(namesdom());
